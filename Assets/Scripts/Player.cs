a0ef542799c851cb3a494e09d55e24a0e6aab43d
Thrisha Kopula <thrishak1@gmail.com>
2022-11-25 10:43:00 -0800

    trying to add 'e' to interact

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,7 +11,9 @@ public class Player : MonoBehaviour
 
     public HungerBar hungerBar;
     public float speed = .1f;
-
+    public Transform cam;
+    public float playerActivateDistance;
+    bool active = false;
     void Start()
     {
         currentHunger = 0;
@@ -20,27 +22,48 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+        active = Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, playerActivateDistance);
+        if(Input.GetKeyDown(KeyCode.E) && active == true)
+        {
+            if(hit.transform.CompareTag("edible"))
+            {
+                Debug.Log("hit");
+                Destroy(hit.collider.gameObject);
+                AddHunger(1);
+            }
 
+            if (hit.transform.CompareTag("toxic"))
+            {
+                Destroy(hit.collider.gameObject);
+                restartLevel();
+            }
+        }
         float xDirection = Input.GetAxis("Horizontal");
         float zDirection = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
         transform.position += moveDirection * speed;
 
+
+
     }
     private void OnTriggerEnter(Collider collision)
     {
+        /*
         if (collision.gameObject.CompareTag("edible"))
         {
             Destroy(collision.gameObject);
             AddHunger(1);
         }
+        
 
         if (collision.gameObject.CompareTag("toxic"))
         {
             Destroy(collision.gameObject);
             restartLevel();
         }
+        */
     }
 
     void AddHunger(int hunger)
